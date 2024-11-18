fc1d22333aa4c1697212b1309ecc79d2ebc7ebb5
tomasp03 <tomasp0303@gmail.com>
2024-11-18 19:20:48 +0100

    Fixed main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <Framer/Camera.h>
-
 #include "Framer/Framer.h"
 #include "imgui.h"
 #include "glm/gtc/type_ptr.hpp"
@@ -8,34 +6,13 @@ int main() {
 
     fr::Window window(800, 800, "Framer-Project");
 
-
-    glm::vec3 color{1.0f};
-
-    glEnable(GL_DEPTH_TEST);
+    glm::vec4 background{0.07f, 0.13f, 0.17f, 1.0f};
     while (window.IsRunning()) {
-        window.ShowFPS();
-        window.BegimFrame();
-
-        ImGui::Begin("Settings");
-        ImGui::ColorPicker3("Color", glm::value_ptr(color));
-        if (ImGui::Checkbox("VSync", &VSync)) {
-            window.SetVSync(VSync);
-        }
-
-        if (!ImGui::IsWindowHovered())
-            camera.OnUpdate();
-
+        window.BegimFrame(background.r, background.g, background.b, background.a);
+        ImGui::Begin("Hello window");
+        ImGui::Text("Background");
+        ImGui::ColorEdit4("Color", glm::value_ptr(background));
         ImGui::End();
-
-        shader.Bind();
-        VAO.Bind();
-
-        shader.uni3f("u_Color" , color.r, color.g, color.b);
-        shader.uni4mat("u_Proj", glm::value_ptr(camera.GetProjectionMatrix()));
-        shader.uni4mat("u_View", glm::value_ptr(camera.GetViewMatrix()));;
-
-        glDrawElements(GL_TRIANGLES, sizeof(cubeIndices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
-
 
 
         window.EndFrame();
